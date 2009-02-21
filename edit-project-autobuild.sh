dadef0c5162d7304acef1c9c3ac4713a9e9e0fae
Jeet Sukumaran <jeetsukumaran@gmail.com>
2009-02-21 17:30:09 -0600

    updated how-to in autoconf editor

--- edit-project-autobuild.sh
+++ edit-project-autobuild.sh
@@ -6,10 +6,9 @@
 #    (a) If file is a new executable, add name to "bin_PROGRAMS" list 
 #        (space separated)
 #    (b) Under "<canonical-progname>_SOURCES", add file. 'canonical-progname'
-#        is the same as the <progname> enterd in the "bin_PROGRAMS" list, but
+#        is the same as the <progname> entered in the "bin_PROGRAMS" list, but
 #        with all non ['A'-'Z', 'a'-'z', '0'-'9', '@'] characters substituted
-#        by an underscore.
-# 2. Run "bootstrap.sh".
+#        by underscores.
 #
 # (2) Adding a Directory to the Build
 # -----------------------------------
@@ -18,7 +17,13 @@
 # 2. In "Makefile.am" of project root, add "rel/path/to/new/dir" to "SUBDIRS"
 #    list.
 # 3. Add "Makefile.am" to new source directory.
-# 4. Run "bootstrap.sh".
+#
+# FINALLY (after any changes to Any "configure.ac" or "Makefile.am" files):
+# -------------------------------------------------------------------------
+# 1. Run "bootstrap.sh".
+# 2. In build directory, run 
+#    "rel/path/to/configure --prefix=`pwd`/install [--enable-debugging]".
+# 3. In build directory, run "Make".
 
 FILES=$(find . -name "configure.ac" -or -name "Makefile.am")
 
