a9f9dfa0176a3d24124a1024c4dda4ec2df3c1d5
Jeet Sukumaran <jeetsukumaran@gmail.com>
2009-02-22 02:57:17 -0600

    why are my vectors returning negative sizes?

--- gingkocc/prototypes/unmanaged-ind.cpp
+++ gingkocc/prototypes/unmanaged-ind.cpp
@@ -26,6 +26,31 @@
 #include <list>
 #include <iostream>
 #include <string>
+#include <cmath>
+
+///////////////////////////////////////////////////////////////////////////////
+// SUPPORT FUNCTIONS
+
+double random_uniform() {
+    return (double)rand()/double(RAND_MAX);
+}
+
+int poisson_rv(int rate) {
+    const int MAX_EXPECTATION = 64;
+    if (rate > MAX_EXPECTATION) {
+        double r = rate/2.0;
+        return poisson_rv(r) + poisson_rv(r);
+    }
+    double L = exp(-1.0 * rate);
+    double p = 1.0;
+    double k = 0.0;    
+    while (p >= L) {
+        k += 1.0;
+        p *= random_uniform();
+    }
+    return k - 1.0;
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // POPULATION ECOLOGY AND GENETICS
@@ -41,7 +66,11 @@ typedef std::vector<float> Genotype;
 /// histories. 
 class Individual {
     public:
-        
+        /// default constructor
+        Individual() {
+            // TODO!
+        }            
+                   
         /// instantiates individual with given genotype
         Individual(const Genotype& g)
             : genotype(g) {
@@ -66,6 +95,12 @@ class Population {
             : species(sp),
               cell(c) {
         }
+        void add_individual(const Individual& individual) {
+            this->individuals.push_back(individual);
+        }
+        int size() {
+            return this->individuals.size(); 
+        }
     
     private:
         const Species*    species; // the species to which this population belongs
@@ -81,16 +116,39 @@ class Species {
         Species(const char* sp_label) 
             : label(sp_label) {       
         }
-        const Population get_population();
+        virtual ~Species() {}
+        virtual Population get_population(Cell* cell=NULL, int max_size=0) const;
         
     private:
-        std::string      label;
+        std::string label;
 //         std::list<Population*> populations;
 }; // Species
 
 typedef std::vector<Species> SpeciesContainer;
 typedef std::vector<Species>::iterator SpeciesIterator;
 typedef std::vector<Species>::const_iterator SpeciesConstIterator;
+
+/// Returns a Population object with the Population object's Species pointer
+/// set to self.
+/// If Cell* is given, then the Population object's Cell pointer is set.
+/// If max_size > 0, then individuals are created and added to the population,
+/// with the number of individuals >= 0 and <= max_size.
+/// The exact number of individuals is currently implemented as a Poisson 
+/// distributed random number with mean of max_size. 
+/// Future implementations and/or derived classes will take into account the 
+/// environment of cell, with favorable environments resulting in greater 
+/// numbers of individuals.
+Population Species::get_population(Cell* cell, int max_size) const {
+    Population p = Population(this, cell);
+    if (max_size > 0) {
+        int n = poisson_rv(max_size);
+        std::cout << n << std::endl;
+        for (; n > 0; --n) {
+            p.add_individual(Individual());
+        }
+    }
+    return p;
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 // SPATIAL FRAMEWORK
@@ -113,6 +171,11 @@ class Cell {
         }
         
         void initialize_populations();
+        
+        // for debugging
+        int num_individuals() {
+            return this->populations[0].size();
+        }
     
     private:
         int                        carrying_capacity;
@@ -129,7 +192,7 @@ void Cell::initialize_populations() {
     for (SpeciesConstIterator sp=this->species->begin(); 
             sp != this->species->end();
             sp++) {
-        this->populations.push_back(Population(&(*sp), this)); 
+        this->populations.push_back(sp->get_population(this, this->carrying_capacity)); 
     }            
 }
 
@@ -143,6 +206,15 @@ class World {
         void set_cell_carrying_capacity(int carrying_capacity);
         void add_species(const Species& sp);
         void initialize_biota();
+        
+        // for debugging
+        void dump(std::ostream& out) {
+            int count=1;
+            for (CellIterator i=this->cells.begin(); 
+                i != this->cells.end(); ++i, ++count) {
+                    out << count << ": " << i->num_individuals() << "\n";
+            }            
+        }
         
     private:
         int                 dim_x;
@@ -211,6 +283,7 @@ int main(int argc, char * argv[]) {
 	world.generate_landscape(dim_x, dim_y);
 	world.set_cell_carrying_capacity(cc);
 	world.add_species(Species("snail"));
+	world.dump(std::cout);
 	return 0;
 }
 
