9984678f64d9da0b9cf4cfd2363bc78acaf34f29
Jeet Sukumaran <jeetsukumaran@gmail.com>
2009-03-21 18:14:37 -0500

    test for nodes allocated on heap

--- gingkocc/tests/test_tree_from_genealogies.cpp
+++ gingkocc/tests/test_tree_from_genealogies.cpp
@@ -25,6 +25,21 @@
 #include <string>
 #include <sstream>
 
+/*
+                   0
+                  / \
+                 1   2
+                / \   \
+               3   4   5
+              / \    \   \
+             6    7   8   9
+            / \  / \   \   \  
+           a   b c d    e   f
+
+
+          ((((a:1,b:1):1,(c:1,d:1):1):1,e:3):1,f:4):1;
+
+*/  
 
 std::string build_tree1() {
     gingko::GenealogyNode n0;
@@ -78,23 +93,77 @@ std::string build_tree1() {
     return s.str();
 }
 
+std::string build_tree2() {
+    gingko::GenealogyNode * n0;
+    gingko::GenealogyNode * n1 = new gingko::GenealogyNode();
+    gingko::GenealogyNode * n2 = new gingko::GenealogyNode();   
+    n1->link(n0);
+    n2->link(n0);
+    gingko::GenealogyNode * n3 = new gingko::GenealogyNode();
+    gingko::GenealogyNode * n4 = new gingko::GenealogyNode();
+    gingko::GenealogyNode * n5 = new gingko::GenealogyNode();
+    n3->link(n1);
+    n4->link(n1);
+    n5->link(n2);
+    gingko::GenealogyNode * n6 = new gingko::GenealogyNode();
+    gingko::GenealogyNode * n7 = new gingko::GenealogyNode();
+    gingko::GenealogyNode * n8 = new gingko::GenealogyNode();
+    gingko::GenealogyNode * n9 = new gingko::GenealogyNode();
+    n6->link(n3);
+    n7->link(n3);
+    n8->link(n4);
+    n9->link(n5);
+    gingko::GenealogyNode * na = new gingko::GenealogyNode();
+    gingko::GenealogyNode * nb = new gingko::GenealogyNode();
+    gingko::GenealogyNode * nc = new gingko::GenealogyNode();
+    gingko::GenealogyNode * nd = new gingko::GenealogyNode();
+    gingko::GenealogyNode * ne = new gingko::GenealogyNode();
+    gingko::GenealogyNode * nf = new gingko::GenealogyNode();    
+    na->link(n6);
+    nb->link(n6);
+    nc->link(n7);
+    nd->link(n7);
+    ne->link(n8);
+    nf->link(n9);
+    
+    gingko::Tree tree;
+    
+    std::string label_a("a");
+    tree.process_node(na, &label_a);    
+    std::string label_b("b");
+    tree.process_node(nb, &label_b);
+    std::string label_c("c");
+    tree.process_node(nc, &label_c);
+    std::string label_d("d");
+    tree.process_node(nd, &label_d);
+    std::string label_e("e");
+    tree.process_node(ne, &label_e);
+    std::string label_f("f");
+    tree.process_node(nf, &label_f);
+    std::ostringstream s;
+    tree.write_newick_tree(s);
+    
+    delete nf;
+    delete ne;
+    delete nd;
+    delete nc;
+    delete nb;
+    delete na;
+    delete n9;
+    delete n8;
+    delete n7;
+    delete n6;
+    delete n5;
+    delete n4;
+    delete n3;
+    delete n2;
+    delete n1;        
+    
+    return s.str();
+}
+
 int main(int, char *) {
 
-/*
-                   0
-                  / \
-                 1   2
-                / \   \
-               3   4   5
-              / \    \   \
-             6    7   8   9
-            / \  / \   \   \  
-           a   b c d    e   f
-
-
-          ((((a:1,b:1):1,(c:1,d:1):1):1,e:3):1,f:4):1;
-
-*/  
     std::string expected = "((((a:1,b:1):1,(c:1,d:1):1):1,e:3):1,f:4):1;";
 
     std::string tree1 = build_tree1();
@@ -104,7 +173,18 @@ int main(int, char *) {
         std::cerr << expected << std::endl;
         std::cerr << "Observed:" << std::endl;
         std::cerr << tree1 << std::endl;
+        exit(1);
     }
+    
+    std::string tree2 = build_tree1();
+    if (expected.compare(tree2) != 0) {
+        std::cout << "FAIL" << std::endl;
+        std::cerr << "Expecting:" << std::endl;
+        std::cerr << expected << std::endl;
+        std::cerr << "Observed:" << std::endl;
+        std::cerr << tree2 << std::endl;
+        exit(1);
+    }    
     
     std::cout << "SUCCESS" << std::endl;
 }
