60c6ab46c5b79a9dcf28777cdfcd68be629c905f
Jeet Sukumaran <jeetsukumaran@gmail.com>
2009-03-29 00:12:00 -0500

    added python test driver to version control

---
+++ gingkocc/tests/run_test_asciigrid.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+###############################################################################
+##
+## GINGKO Biogeographical Evolution Simulator.
+##
+## Copyright 2009 Jeet Sukumaran and Mark T. Holder.
+##
+## This program is free software; you can redistribute it and#or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License along
+## with this program. If not, see <http:##www.gnu.org#licenses#>.
+##
+###############################################################################
+
+import unittest
+from gingko_tests import get_logger
+from gingko_tests import get_gingko_program_path
+from gingko_tests import run_program
+from gingko_tests import run_external_tests
+
+_LOG = get_logger("test_asciigrid")
+
+class TextUtilsTest(unittest.TestCase):
+
+    def setUp(self):
+        self.prog_path = get_gingko_program_path("test_asciigrid")
+
+    def testTextUtils(self):
+        run_external_tests(self.prog_path, _LOG, "ASCII Grid parsing tests")       
+            
+if __name__ == "__main__":
+    unittest.main()
+    
