63ef9ec383d9311b78a584b9e2f9b6a4fae145d1
Jeet Sukumaran <jeetsukumaran@gmail.com>
2009-05-20 11:04:37 +0700

    added gingkopy skeleton

---
+++ gingkopy/README.txt
@@ -0,0 +1,12 @@
+Gingko Post-Processing Python Library
+======================================
+
+(C) 2008 Jeet Sukumaran and Mark T. Holder.
+
+The code is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+
+For installation information, please see INSTALL.txt
---
+++ gingkopy/setup.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python
+
+###############################################################################
+##
+## GINGKO Biogeographical Evolution Simulator Post-Processing Library.
+##
+## Copyright 2009 Jeet Sukumaran and Mark T. Holder.
+##
+## This program is free software; you can redistribute it and#or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License along
+## with this program. If not, see <http:##www.gnu.org#licenses#>.
+##
+###############################################################################
+
+"""
+Package setup and installation.
+"""
+
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup
+from setuptools import find_packages
+from gingkopy import PACKAGE_VERSION
+
+import sys
+import os
+import subprocess
+
+script_names = []
+setup(name='GingkoPy',
+      version=PACKAGE_VERSION,     
+      author='Jeet Sukumaran and Mark T. Holder',
+      author_email='jeet@ku.edu and mtholder@ku.edu',
+      url='http://sourceforge.net/projects/dendropy/',
+      description="""\
+GINGKO Biogeographical Evolution Simulator Post-Processing Library""",
+      license='GPL 3+',
+      packages=['gingkopy'],
+      package_dir={'gingkopy': 'gingkopy'},
+      package_data={
+        "" : ['doc/*'],
+        "gingkopy" : ["tests/data/*"]
+      },
+      scripts = [('scripts/%s' % i) for i in script_names],
+      test_suite = "gingkopy.tests",
+      include_package_data=True,         
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,      
+      long_description="""\
+A Python library for processing GINGKO output""",
+      classifiers = [
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: GNU General Public License (GPL)",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Topic :: Scientific/Engineering :: Bio-Informatics",
+            ],
+      keywords='phylogenetics evolution biology biogeography',      
+      )
