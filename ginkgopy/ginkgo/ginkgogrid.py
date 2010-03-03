f08003685df767c8199ec09c63892a76ebf701bb
Jeet Sukumaran <jeetsukumaran@gmail.com>
2010-03-02 22:56:11 -0600

    tweaks and enhancements to grid generator script

--- ginkgopy/ginkgo/ginkgogrid.py
+++ ginkgopy/ginkgo/ginkgogrid.py
@@ -19,10 +19,22 @@ class Grid(object):
             self.populate(kwargs['func'])
         else:
             self.values = {}
-        self.is_int = kwargs.get('is_int', False)
+        self._max_formatted_value_len = None
 
-    def format_value(self, value):
-        return str(value)
+    def formatted_values(self):
+        fv = {}
+        fv_lens = []
+        for x in range(self.ncols):
+            fv[x] = {}
+            for y in range(self.nrows):
+                v = self.values[x][y]
+                if isinstance(v, float):
+                    fv[x][y] = "{0:>.4}".format(v)
+                else:
+                    fv[x][y] = "{0:>}".format(v)
+                fv_lens.append(len(fv[x][y]))
+        self._max_formatted_value_len = max(fv_lens)
+        return fv
 
     def populate(self, func):
         self.values = {}
@@ -41,46 +53,30 @@ cellsize      50.0""").format(self.ncols, self.nrows)
     def __str__(self):
         rows = []
         rows.append(self.grid_header())
-        max_field_len = 8
+        fv = self.formatted_values()
         for y in range(self.nrows):
             if y % 5 == 0:
                 rows.append("")
             row = []
             for x in range(self.ncols):
-                leader = " " if (x and (x % 5 == 0)) else ""
-                v = self.values[x][y]
-                if isinstance(v, float):
-                    row.append("{2}{0:>{1}.4}".format(v, max_field_len, leader))
-                else:
-                    row.append("{2}{0:>{1}}".format(v, max_field_len, leader))
+#                leader = ("{0:{1}}".format(" ", self._max_formatted_value_len)) if (x and (x % 5 == 0)) else ""
+                leader = "   " if (x and (x % 5 == 0)) else ""
+                v = fv[x][y]
+                row.append("{2}{0:>{1}}".format(v, self._max_formatted_value_len, leader))
             rows.append("  ".join(row))
         return "\n".join(rows)
-
-class RealGrid(Grid):
-
-    def __init__(self, ncols, nrows, **kwargs):
-        Grid.__init__(self, ncols, nrows, **kwargs)
-        self.is_int = False
-
-class IntGrid(Grid):
-
-    def __init__(self, ncols, nrows, **kwargs):
-        Grid.__init__(self, ncols, nrows, **kwargs)
-        self.is_int = True
 
 ###############################################################################\\
 # Build and return the grids
 
 def random_gaussian_grid(ncols, nrows, mean=0, sd=1):
-    return RealGrid(ncols, nrows, func = lambda x, y: random.gauss(mean, sd))
+    return Grid(ncols, nrows, func = lambda x, y: random.gauss(mean, sd))
 
 def random_uniform_real_grid(ncols, nrows, a, b):
-    return RealGrid(ncols, nrows, func = lambda x, y: random.uniform(a, b))
+    return Grid(ncols, nrows, func = lambda x, y: random.uniform(a, b))
 
 def random_uniform_int_grid(ncols, nrows, a, b):
-    print a
-    print b
-    return IntGrid(ncols, nrows, func = lambda x, y: random.randint(a, b))
+    return Grid(ncols, nrows, func = lambda x, y: random.randint(a, b))
 
 def fixed_value_grid(ncols, nrows, val):
     return Grid(ncols, nrows, func = lambda x, y: val)
